5197f22f4254e90d9f512c0fb94f4f98a356aac0
akshayrock45 <77655354+akshayrock45@users.noreply.github.com>
2023-12-11 17:22:00 +0530

    Add files via upload
    Initial commit

---
+++ smartinventory/smartinventory/src/test/java/com/incedo/capstone/smartinventory/SmartinventoryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.incedo.capstone.smartinventory;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartinventoryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
