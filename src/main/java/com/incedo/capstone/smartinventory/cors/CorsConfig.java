2aa5afb7a37e33b06f8bc94ac91a04b75c79c215
Subham Saha <subhams56.ss@gmail.com>
2023-12-14 22:46:14 +0530

    Cors Config added

---
+++ src/main/java/com/incedo/capstone/smartinventory/cors/CorsConfig.java
@@ -0,0 +1,21 @@
+package com.incedo.capstone.smartinventory.cors;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+@EnableWebMvc
+public class CorsConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("http://127.0.0.1:5173", "http://localhost:5173")
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                .allowedHeaders("Origin", "Content-Type", "Accept", "Authorization")
+                .allowCredentials(true)
+                .maxAge(3600);
+    }
+}
